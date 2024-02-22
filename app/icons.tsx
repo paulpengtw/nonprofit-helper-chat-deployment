3d68341f8f0bad2798682fff337ad23bac84a0f8
CHENG PENG <paulpeng118@gmail.com>
2024-02-22 14:23:15 +0800

    icon fix again

--- app/icons.tsx
+++ app/icons.tsx
@@ -5882,7 +5882,7 @@ export const GithubIcon = () => {
 export const g0vIcon = () => {
   return (
     <svg version="1.1"  xmlns="http://www.w3.org/2000/svg"
-	 width="20" viewBox="0 0 256 256" enable-background="0 0 256 256">
+	 width="100%" viewBox="0 0 256 256">
 <path fill="#FFFEFE" opacity="1.000000" stroke="none" 
 	d="
 M244.000000,0.999999 
