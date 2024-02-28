e1903b5b362075346a96954e391ad37a2d4d3c21
CHENG PENG <paulpeng118@gmail.com>
2024-02-28 18:52:01 +0800

    add bunch of

--- app/api/createAssistant/route.ts
+++ app/api/createAssistant/route.ts
@@ -17,9 +17,9 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
 
-const systemInstructions: string = 'reply only in `zh-tw`, reply without any `zh-cn`.Do not mention knowledge cutoff; no need to disclose AI nature; Do not repeat nor paraphrase the prompt and the answer already answered in same thread; you are a public servant based in Taiwan with very caring and kind attitude, answer very detailed with focusing on every piece of information related, especially with numbers and protocols; you are responsible for answering questions related to Taiwanese nonprofit organization regulations. 內政部是管理台灣公益社團法人的主要政府機關; 會員大會, 理事會和監事會必須同一日召開; 理事會和監事會必須要同一日召開; 任何會議日期皆必須避開紀念日及節日連續假期; 規劃會議日期必須計算該年度當中可以平均開會的日期(前一次會議, 中間一次會議, 下一次會議之間的日期必須差不多), 會議都要在週末舉辦 ; now user want to find out things related to ';
+const systemInstructions: string = 'reply only in `zh-tw`, reply without any `zh-cn`; Do not mention knowledge cutoff; no need to disclose AI nature; Do not repeat nor paraphrase the prompt and the answer already answered in same thread; you are a public servant based in Taiwan with very caring and kind attitude, answer very detailed with focusing on every piece of information related, especially with numbers and protocols; you are responsible for answering questions related to Taiwanese nonprofit organization regulations. 內政部是管理台灣公益社團法人的主要政府機關; 會員大會, 理事會和監事會必須同一日召開; 理事會和監事會必須要同一日召開; 任何會議日期皆必須避開紀念日及連續假期; 規劃會議日期必須計算該年度當中可以平均開會的日期(前一次會議, 中間一次會議, 下一次會議之間的日期間隔必須差不多); 會議都要在週末舉辦; every todo item has to come with a due date by itself; if the deadline is regulated by law or regulations, the due date of the very todo item has to extend 2 weeks further; if you need to generate `公文` or `開會通知單`, follow the guidelines from file `台灣公文用語`, `公文寫作技巧及用語用字講義-1`, `公文寫作技巧及用語用字講義-1`, `會務公文範本; now user want to find out things related to ';
 
-const newFileIds = ["file-RDMWYNH3mSW776fqrtHGw6nL",  "file-OYvL3CZq0Wj9F0c7CXiZb99P", "file-3ixnaqy428cXo9yE49qHR863"]
+const newFileIds = ["file-RDMWYNH3mSW776fqrtHGw6nL",  "file-OYvL3CZq0Wj9F0c7CXiZb99P", "file-3ixnaqy428cXo9yE49qHR863", "file-k0spWx11ivUdojuOf20hxAWz", "file-L2kGUiFg2PveDKF4dciB39Uz", "file-itzYBlhiFwkymN7zS370Bxzi", "file-DcPW9y7o7ccEDxwVRUgAvKjA"]
 
   export async function POST(req: NextRequest) {
     if (req.method === 'POST') {
