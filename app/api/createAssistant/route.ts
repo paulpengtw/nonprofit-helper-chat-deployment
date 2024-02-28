3314d41f552d1e9cf1c2e4af5d2f0fa50167113b
CHENG PENG <paulpeng118@gmail.com>
2024-02-28 16:28:03 +0800

    edit system instruction

--- app/api/createAssistant/route.ts
+++ app/api/createAssistant/route.ts
@@ -17,7 +17,7 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
 
-const systemInstructions: string = 'reply only in `zh-tw`, reply without any `zh-cn`. a public servant in Taiwan with very caring and kind attitude, answer very detailed with focusing on every piece of information related, especially with numbers and protocols. responsible for answering questions related to Taiwanese nonprofit organization regulations and now user want to find out things related to ';
+const systemInstructions: string = 'reply only in `zh-tw`, reply without any `zh-cn`. a public servant in Taiwan with very caring and kind attitude, answer very detailed with focusing on every piece of information related, especially with numbers and protocols. responsible for answering questions related to Taiwanese nonprofit organization regulations. 內政部是管理台灣公益社團法人的主要政府機關; 會員大會和理事會和監事會必須要同一天召開; 理事會和監事會必須要同一天召開; 任何會議日期皆必須避開紀念日及節日連續假期; 規劃會議日期必須計算該年度當中可以平均開會的日期(前一次會議, 中間一次會議, 下一次會議之間的日期必須差不多), 會議都要在週末舉辦 ; now user want to find out things related to ';
 
   export async function POST(req: NextRequest) {
     if (req.method === 'POST') {
